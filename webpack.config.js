5623415a0612f0c38a57092ab65c8b2afc2fe466
awroz <awroz.dev@gmail.com>
2023-03-28 11:36:46 -0500

    avanzado hasta la clase 9

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const copyWebpackPlugin = require ('copy-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output:{
+        path: path.resolve(__dirname,'dist'),
+        filename:'main.js'
+    },
+    resolve:{
+        extensions:['.js']
+    },
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader'
+            }
+          },
+          {
+            test: /\.css$/i,
+            use:[MiniCssExtractPlugin.loader,'css-loader'],
+          }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            inject: true,
+            title: 'My App',
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new miniCssExtractPlugin(),
+        new copyWebpackPlugin({
+          patterns:[
+            {
+              from: path.resolve(__dirname,'src','assets/images'),
+              to:'assets/images'
+            }
+          ]
+        })
+      ]
+}
